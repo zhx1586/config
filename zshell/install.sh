964e1a266eb6d7f32743b8e7517f9dc9a3659f3f
zhx1586 <zhx1586@126.com>
2021-05-29 23:29:20 +0800

    fix p10k

--- zshell/install.sh
+++ zshell/install.sh
@@ -10,3 +10,8 @@ if [ -f $HOME/.zshrc ]; then
 	mv $HOME/.zshrc $HOME/.zsh-bak/zshrc-bak-`date +%Y%m%d%H%M%S`
 fi
 ln -s $ZSH_CONFIG_PATH/zshrc $HOME/.zshrc
+
+if [ -f $HOME/.p10k.zsh ]; then 
+	mv $HOME/.p10k.zsh $HOME/.zsh-bak/p10k.zsh-bak-`date +%Y%m%d%H%M%S`
+fi
+ln -s $ZSH_CONFIG_PATH/p10k.zsh $HOME/.p10k.zsh
