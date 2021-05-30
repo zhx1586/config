8293edddd35847f15316f30e876d6c157d95500a
zhx1586 <zhx1586@126.com>
2021-05-30 23:17:38 +0800

    fix neovim install.sh

--- neovim/install.sh
+++ neovim/install.sh
@@ -6,7 +6,7 @@ if [ ! -d $HOME/.config ]; then
 	mkdir $HOME/.config
 fi
 
-if [ -d $HOME/.config/nvim ]; then 
+if [ -f $HOME/.config/nvim ]; then 
 	mv $HOME/.config/nvim $HOME/.config/nvim-bak-`date +%Y%m%d%H%M%S`
 fi
 ln -s $NVIM_CONFIG_PATH/nvim $HOME/.config/nvim
